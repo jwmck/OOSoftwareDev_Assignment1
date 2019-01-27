57c9aabeef2da11b10990fc50fc9fbfae3801526
James McKeon <james.mckeon@mail.itsligo.ie>
2019-01-27 17:45:11 +0000

    Add project files.

---
+++ ConsoleApp1/Employee.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Employee
+    {
+        #region Properties
+        //Read-only property of EmployeeSalary limits changes to take place via class method PayRise
+        public string EmployeeFirstName { get; set; }
+        public string EmployeeLastName { get; set; }
+        public string EmployeeID { get; set; }
+        public string EmployeeDepartment { get; set; }
+        private decimal employeeSalary;
+        public decimal EmployeeSalary
+        {
+            get
+            {
+                return employeeSalary;
+            }
+        }
+        #endregion Properties
+
+        #region Constructors
+        //Only one constructor needed as all arguments are required to create an Employee. 
+        //Feedback reommended using 3 as a rule of thumb
+        public Employee(string employeeFirstName, string employeeLastName, string employeeID,
+            string employeeDepartment, decimal initialSalary)
+        {
+            EmployeeFirstName = employeeFirstName;
+            EmployeeLastName = employeeLastName;
+            EmployeeID = employeeID;
+            EmployeeDepartment = employeeDepartment;
+            employeeSalary = initialSalary;
+        }//end of main constructor
+
+        public Employee()
+        {
+
+        }//end of zero argument constructor, he also recommended one more constructor
+        #endregion Contructors
+
+        #region Methods
+        //This ToString method returns all employee information when called
+        public override string ToString()
+        {
+            return string.Format("Information on Employee ID No: {0}\nFirst Name: {1}\nSurname: {2}" +
+                "\nDepartment: {3}\nSalary: {4:C}",
+                EmployeeID, EmployeeFirstName, EmployeeLastName, EmployeeDepartment, EmployeeSalary);
+        }//end of toString method
+
+        //method takes percentageIncrease as argument and increases employeeSalary accordingly
+        public void PayRise(int percentageIncrease)
+        {
+            employeeSalary += (employeeSalary * percentageIncrease/100);
+        }//end of PayRise class method
+
+        #endregion Methods
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Body
+
+            //Ensures correct character set available for currency
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            //Creates employee1 using requestInfo and requestSalary methods
+            Employee employee1 = new Employee(requestInfo("Employee 1's First name"),
+                requestInfo("Employee 1's Surname"), requestInfo("Employee 1's ID"),
+                requestInfo("Employee 1's Department"), requestSalary("Employee 1's Salary"));
+
+            Console.WriteLine("Employee 1 Registered, please press Enter to continue.");
+            Console.ReadLine();
+            Console.Clear();
+
+            //Creates employee2 using requestInfo and requestSalary methods
+            Employee employee2 = new Employee(requestInfo("Employee 2's First name"),
+                requestInfo("Employee 2's Surname"), requestInfo("Employee 2's ID"),
+                requestInfo("Employee 2's Department"), requestSalary("Employee 2's Salary"));
+
+            Console.WriteLine("Employee 2 Registered, please press Enter to continue.");
+            Console.ReadLine();
+            Console.Clear();
+
+            //displays information on employee1
+            displayInfo(employee1);
+
+            //displays information on employee2
+            displayInfo(employee2);
+
+            //request percentage salary increase for Employee1
+            int employee1Increase;
+            employee1Increase = requestIncrease("Employee 1");
+
+            //request percentage salary increase for Employee2
+            int employee2Increase;
+            employee2Increase = requestIncrease("Employee 2");
+
+            //apply pay rise to employee1
+            employee1.PayRise(employee1Increase);
+
+            //apply pay rise to employee2
+            employee2.PayRise(employee2Increase);
+
+            //display info of employee1
+            displayInfo(employee1);
+
+            //display info of employee2
+            displayInfo(employee2);
+
+            #endregion Body
+        }
+
+        #region Methods
+
+        //Method to request information on employee in string format
+        public static string requestInfo(string info)
+        {
+            Console.WriteLine("Please enter {0}:", info);
+            return Console.ReadLine();
+        }//end of requestInfo method
+
+        //method to request employee salary information, repeats until acceptable input given
+        public static decimal requestSalary(string info)
+        {
+            Console.WriteLine("Please enter {0}:", info);
+            decimal salaryAmount;
+            //this if-else statement tests input and repeats until an acceptable user input
+            if (decimal.TryParse(Console.ReadLine(), out salaryAmount) == true)
+            {
+                return salaryAmount;
+            }//end of if true
+            else
+            {
+                Console.WriteLine("Invalid data entered, please try again.");
+                return requestSalary(info);
+            }//end of else
+
+        }//end of requestSalary method
+
+        //method to request salary increase percentage, repeats until acceptable input given
+        public static int requestIncrease(string employee)
+        {
+            Console.WriteLine("Please enter the percentage pay rise," +
+                    " without decimals, for {0}", employee);
+
+            int increaseAmount;
+            //this if-else statement tests input and repeats until an acceptable user input
+            if (int.TryParse(Console.ReadLine(), out increaseAmount) == true)
+            {
+                Console.WriteLine("Thank you, please press Enter to continue.");
+                Console.ReadLine();
+                Console.Clear();
+                return increaseAmount;
+            }//end of if true
+            else
+            {
+                Console.WriteLine("Invalid data entered.");
+                return requestIncrease(employee);
+            }//end of else
+
+        }//end of requestIncrease method
+
+        //method to display info on employee, this method calls the ToString() function
+        public static void displayInfo(Object employee)
+        {
+            Console.WriteLine(employee);
+            Console.WriteLine("Please press Enter to continue.");
+            Console.ReadLine();
+            Console.Clear();
+        }
+        #endregion Methods
+    }//end of displayInfo method
+}
